304729fd43df2f0925c332b83620686bc08f9df7
Edison Wu <ewu@digitalriver.com>
2019-05-23 14:26:26 +0800

    return payload

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import CloudPayPayPal from './cloudpay-paypal';
 import CloudPayCheckout from './cloudpay-checkout';
 import Page from './page-model';
 import { getAccessToken } from './utils';
+import CreditCardPayload from './payload/creditcard-payload';
+import PayPalPayload from './payload/paypal-payload';
+import DirectDebitPayload from './payload/directdebit-payload';
 
 (function(window) {
+  const page = new Page();
+
   const init = async (params) => {
     const {siteInfo = {}, digitalriverJS, paymentMethods = [], options = {}} = params || {};
     let creditCardElements = null;
-    const page = new Page();
 
     if (Object.keys(siteInfo).length === 0) {
       throw new Error('Please pass the site info object.');
@@ -49,7 +53,54 @@ import { getAccessToken } from './utils';
     return creditCardElements;
   };
 
+  const buildPayload = async (siteInfo, type) => {
+    if (Object.keys(siteInfo).length === 0) {
+      throw new Error('Please pass the site info object.');
+    }
+
+    if (typeof type !== 'string') {
+      throw new Error('Please pass the payment type.');
+    }
+
+    let payload = {};
+
+    switch (type) {
+      case 'creditCard': {
+        const CP = new CreditCardPayload(siteInfo, page);
+        
+        payload = CP.buildPayload();
+
+        break;
+      }
+      case 'payPal': {
+        const PP = new PayPalPayload(siteInfo, page);
+
+        try {
+          payload = await PP.buildPayload();
+        } catch (error) {
+          console.error(error.message);
+        }
+
+        break;
+      }
+      case 'directDebit': {
+        const DD = new DirectDebitPayload(siteInfo, page);
+
+        payload = DD.buildPayload();
+
+        break;
+      }
+      default: {
+        throw new Error('No matched type.');
+      }
+    }
+
+    return payload;
+  };
+
+
   window.DRCloudPay = {
-    init: init
+    init: init,
+    buildPayload: buildPayload
   };
 })(window);
